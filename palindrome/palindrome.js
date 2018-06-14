839a46bcbfbccc431d45a1edd4f03d2886aea708
Jennifer Bland <jennifer.bland@sbdinc.com>
2018-06-14 09:17:15 -0400

    Updated palindrome code

---
+++ palindrome/palindrome.js
@@ -0,0 +1,27 @@
+/***************************************************************
+ *                                                             *
+ *                       CodePrep.io                           *
+ *                                                             *
+ *   Function:  palindrome(str)                                *
+ *   Input:    1 string parameter                              *
+ *   Output:   boolean                                         *
+ *                                                             *
+ *   Output expectations:                                      *
+ *   var myTest = palindrome('civic');                         *
+ *   console.log(myTest);  // true                             *
+ *                                                             *
+ *   A palindrome is a word, phrase or number which            *
+ *   reads the same backward or forward.                       *
+ *                                                             *
+ *   Write the code for the palindrome function that           *
+ *   accepts a string input and then returns true or           *
+ *   false if that string is a palindrome. Allowances          *
+ *   must be made for capital letters, punctuation and         *
+ *   word dividers.                                            *
+ *                                                             *
+ **************************************************************/
+
+var palindrome = function(str) {
+    /* YOUR CODE GOES HERE */
+    return str.toLowerCase() === str.split('').reverse().join('');
+};
