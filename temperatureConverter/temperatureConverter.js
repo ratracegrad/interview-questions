c5b8971d41b286433583dc760a015edd20e09bbf
Jennifer Bland <jennifer.bland@sbdinc.com>
2018-06-30 10:34:46 -0400

    Changed spelling

--- temperatureConverter/temperatureConverter.js
+++ temperatureConverter/temperatureConverter.js
@@ -2,34 +2,36 @@
  *                                                             *
  *                       CodePrep.io                           *
  *                                                             *
- *   Function:  temperatureConvertor(num, str)                 *
+ *   Function:  temperatureConverter(num, str)                 *
  *   Input:    1 number parameter and 1 string parameter       *
  *   Output:   number to one decimal place                     *
  *                                                             *
  *   Output expectations:                                      *
- *   var myTest = temperatureConvertor(80, 'c');               *
+ *   var myTest = temperatureConverter(80, 'c');               *
  *   console.log(myTest);  // 26.7                             *
  *                                                             *
  *                                                             *
- *   Write the code for the temperaturConvertor function       *
+ *   Write the code for the temperatureConverter function      *
  *   that accepts two parameters: a number and a a string.     *
  *   The first parameter is a temperature reading. The         *
  *   second parameter is single character that is the          *
- *   temperature.  The values are either 'F' for Farenheit     *
- *   and 'C' if the temperature is in Celsius.                 *
+ *   temperature.  The values are either 'F' for Fahrenheit    *
+ *   and 'C' if the temperature is in Celsius. The second      *
+ *   parameter can be upper or lowercase.                      *
  *                                                             * 
  *   The function will convert the temperature                 *
  *   to the equivalent value in the opposite temperature.      *
- *   (i.e. you convert Farenheight to Celsius and vice versa)  *
+ *   (i.e. you convert Fahrenheit to Celsius and vice versa)   *
  *   The function will return the converted value to one       *
  *   decimal place.                                            *
  *                                                             *
  *   If no value is given for second parameter then assume     *
- *   the given temperature is in Farenheit.                    *
+ *   the given temperature is in Fahrenheit and you convert    *
+ *   it to Celsius.                                            *
  *                                                             *
  **************************************************************/
 
-module.exports= function temperatureConvertor(str) {
+module.exports= function temperatureConverter(str) {
     /* YOUR CODE GOES HERE */
 
 };
