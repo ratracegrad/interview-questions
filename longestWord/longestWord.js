73458942f9fbff0a04d6aab1a5fcaef844090bbd
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-07-03 11:17:56 -0400

    added gitignore and code to check against results

--- longestWord/longestWord.js
+++ longestWord/longestWord.js
@@ -26,4 +26,24 @@
 module.exports= function longestWord(str) {
     /* YOUR CODE GOES HERE */
 
+    function findLongestWord(str) {
+        //Split the string into an array of strings
+        var strSplit = str.split(' ');
+        // var strSplit = "The happy cat jumped on the couch when he saw his mom".split(' ');
+        // var strSplit = ["The", "happy", "cat", "jumped", "on", "the", "couch", "when", "he", "saw", "his", "mom"];
+
+        var longestWord = 0;
+        for(var i = 0; i < strSplit.length; i++){
+            // If strSplit[i].length is greater than the word it is compared to
+            //the longestWord takes this new value
+            if(strSplit[i].length > longestWord){
+                longestWord = strSplit[i].length;
+            }
+        }
+        return longestWord;
+    }
+    findLongestWord("The happy cat jumped on the couch when he saw his mom");
+
 };
+
+
--- reverseSentence/reverseSentence.js
+++ reverseSentence/reverseSentence.js
@@ -23,6 +23,29 @@
  **************************************************************/
 
 module.exports= function reverseSentence(str) {
+
+
     /* YOUR CODE GOES HERE */
 
+    function reverseString(str) {
+
+        var splitString = str.split("");
+        //var splitString = "hello".split("");
+        //this should return a split array ["h", "e", "l", "l", "o"]
+
+
+        var reverseArray = splitString.reverse();
+        // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+        // ["o", "l", "l", "e", "h"]
+
+
+        var joinArray = reverseArray.join("");
+        // var joinArray = ["o", "l", "l", "e", "h"].join("");
+        // "olleh"
+
+
+        return joinArray; // "olleh"
+    }
+
+    reverseString("hello");
 };
